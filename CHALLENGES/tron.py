ffaa1df1691bdd3ef47b9d447dcbcd66c8f2f630
jakkyl <pantomime.jakkyl+GIT@gmail.com>
2017-04-01 01:59:44 -0400

    updating folders and adding tron

---
+++ CHALLENGES/tron.py
@@ -0,0 +1,200 @@
+﻿import sys
+import math
+from enum import Enum
+from _collections import deque
+
+class State(Enum):
+    EMPTY = 0
+    WALL = 1
+    PLAYER = 2
+    
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        return
+
+    def direction(self, p):
+        if p.x > self.x: return 'RIGHT'
+        if p.x < self.x: return 'LEFT'
+        if p.y < self.y: return 'UP'        
+        if p.y > self.y: return 'DOWN'
+
+        return 'LEFT'
+
+    def distance(self, p):
+        return abs(p.x - self.x) + abs(p.y - self.y)
+
+    def __str__(self, **kwargs):
+        return "[{},{}]".format(self.x, self.y)
+
+class Player(Point):
+    def __init__(self, id, x0, y0, x1, y1):
+        self.id = id
+        self.head = Point(x0, y0)
+        self.tail = Point(x1, y1)
+
+        super().__init__(x1,y1)
+        return 
+            
+
+class Node(Point):
+    def __init__(self, x, y, state):
+        self.state = state
+        return super().__init__(x, y)
+
+    def neighbors(self):
+        n = []
+
+        if (self.checkNeighbor(self.x - 1, self.y)) : n.append(graph[self.x - 1][self.y])
+        if (self.checkNeighbor(self.x + 1, self.y)) : n.append(graph[self.x + 1][self.y])
+        if (self.checkNeighbor(self.x, self.y - 1)) : n.append(graph[self.x][self.y - 1])
+        if (self.checkNeighbor(self.x, self.y + 1)) : n.append(graph[self.x][self.y + 1])
+
+        return n
+
+    def checkNeighbor(self, x, y):
+        if x < 0 or x > 29 or y < 0 or y > 19: return False
+
+        n = graph[x][y]
+        if n.state == State.WALL or n.state == State.PLAYER: return False
+
+        return True
+
+    def score(self, graph, depth, maxPlayer):       
+        #maxScore = depth
+        #myScore = 0
+        #for i in range(0, len(players), 2):
+        #    p1Nodes = availableNodes(graph, graph[players[i].x][players[i].y])
+        #    p2Nodes = availableNodes(graph, graph[players[i + 1].x][players[i + 1].y])
+        #    if maxPlayer and len(p1Nodes) == 0: 
+        #    score = sum([node.distance(player) for node in nodes])
+        #    p1Nodes = []
+        #    if maxPlayer and player.id == p:
+        #        myScore = score
+        #        score = 0
+        #    else: 
+        #        score = max(score, maxScore)
+        player = self
+
+        myNodes = availableNodes(graph, graph[player.x][player.y])
+        theirNodes = []
+        distance = 0
+        flatGraph = [node for node in sum(graph, []) if node.state == State.PLAYER]
+        others = [pl for pl in flatGraph if pl != player]
+        #print("graph: {} ".format(flatGraph), file=sys.stderr)
+        for pl in others:
+            nodes = availableNodes(graph, graph[pl.x][pl.y])
+            distance += sum([node.distance(pl) for node in nodes])
+            theirNodes = [x for x in nodes if player.distance(x) > pl.distance(x)]
+
+        myNodes = [x for x in myNodes if player.distance(x) < others[0].distance(x)]        
+
+        score = 10000 * len(myNodes) - 10 * len(theirNodes) - distance
+        return score if maxPlayer else -score
+
+    def __str__(self, **kwargs):
+        return "[{}-{},{}]".format(self.state.value, self.x, self.y)
+    
+def availableNodes(graph, start):
+    openSet = deque()
+    openSet.append(start)
+
+    closedSet = []
+    while len(openSet) > 0:
+        current = openSet.popleft()
+
+        for n in current.neighbors():
+            if n not in closedSet: closedSet.append(n)
+            if n not in openSet and n not in closedSet: openSet.append(n)
+
+    return closedSet
+
+#build the map
+graph = []
+for w in range(30):
+    col = []
+    for h in range(20):
+        col.append(Node(w, h, State.EMPTY))
+    graph.append(col)
+
+def negaMax(node, depth, alpha, beta, maximizingPlayer, graph):
+    s = []
+    for i in range(20):        
+        for x in range(30):
+            if graph[x][i].state != State.EMPTY:
+                s.append(" [{}]".format(graph[x][i].state.value))
+    print("{}".format(s), file=sys.stderr)
+
+    if depth == 0 or len(node.neighbors()) == 0:
+        graph[node.x][node.y].state = State.PLAYER
+        return node.score(graph, depth, maximizingPlayer)
+       
+    for neighbor in node.neighbors():
+        temp = neighbor.state
+        v = -negaMax(neighbor, depth - 1, -beta, -alpha,not maximizingPlayer, graph)
+        neighbor.state = temp
+        alpha = max(alpha, v)
+        if alpha >= beta:
+            break
+
+        #print("Score: {} for {}".format(v, neighbor), file=sys.stderr)
+    return alpha
+    
+
+def bestMove(player, graph):
+    bestVal = -math.inf
+    move = player
+    
+    for neighbor in graph[player.x][player.y].neighbors():
+        temp = neighbor.state
+        v = negaMax(neighbor, 7, -math.inf, math.inf,True, graph)
+        
+        print("Score: {} for {}".format(v, neighbor), file=sys.stderr)
+        if v > bestVal:
+            bestVal = v
+            move = neighbor
+
+        neighbor.state = temp
+
+    return move
+
+players = []
+p = 0
+# game loop
+while True:
+    players = []
+    # n: total number of players (2 to 4).
+    # p: your player number (0 to 3).
+    n, p = [int(i) for i in input().split()]
+    for i in range(n):
+        # x0: starting X coordinate of lightcycle (or -1)
+        # y0: starting Y coordinate of lightcycle (or -1)
+        # x1: starting X coordinate of lightcycle (can be the same as X0 if you
+        # play before this player)
+        # y1: starting Y coordinate of lightcycle (can be the same as Y0 if you
+        # play before this player)
+        x0, y0, x1, y1 = [int(j) for j in input().split()]
+        players.append(Player(i, x0, y0, x1, y1))
+        graph[x1][y1].state = State.PLAYER
+
+    #for i in range(20):
+    #    s = ''
+    #    for x in range(30):
+    #        s += " {}".format(graph[x][i])
+    #    print("{}".format(s), file=sys.stderr)
+
+    move = bestMove(players[p], graph)
+    d = players[p].direction(move)
+    
+    graph[move.x][move.y].state = State.PLAYER
+
+    #print("{} {}".format(move, d), file=sys.stderr)
+    
+    # To debug: print("Debug messages...", file=sys.stderr)
+
+    # A single line with UP, DOWN, LEFT or RIGHT
+    print(d)
+
+    for i in range(n):
+        graph[players[i].x][players[i].y].state = State.WALL
